6fb1f44872bffcc6cf9a961ddf19b2547f444f66
Lance Pollard <lancejpollard@gmail.com>
2013-03-30 23:13:44 -0700

    not simple yet, just getting to work

--- index.js
+++ index.js
@@ -5,3 +5,27 @@
 
 var route = require('tower-route');
 
+route.on('define', function(i){
+  callbacks.push(function(context, next){
+    i.handle(context, next);
+  });
+});
+
+var callbacks = [];
+
+module.exports = router;
+
+function router(context, fn) {
+  var i = -1;
+
+  function next() {
+    i++;
+
+    if (!callbacks[i])
+      return fn();
+
+    callbacks[i](context, next);
+  }
+
+  next();
+}
--- test/index.js
+++ test/index.js
@@ -1,11 +1,22 @@
-var towerServerRouter = 'undefined' == typeof window
-  ? require('..')
-  : require('tower-server-router'); // how to do this better?
+var router = require('..')
+  , route = require('tower-route')
+  , Context = require('tower-context')
+  , container = require('tower-container')
+  , assert = require('assert');
 
-var assert = require('assert');
+describe('router', function(){
+  it('should GET /', function(done){
+    route('/', 'index')
+      .use(function(context, next){
+        context.called = true;
+        next();
+      })
 
-describe(towerServerRouter, function() {
-  it('should test', function() {
-    assert.equal(1 + 1, 2);
+    var context = new Context('/', container);
+
+    router(context, function(){
+      assert(context.called);
+      done();
+    });
   });
 });
